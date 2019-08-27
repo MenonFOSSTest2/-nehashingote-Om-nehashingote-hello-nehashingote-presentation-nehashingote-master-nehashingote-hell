155d1e5c607dc234d940017a6febb9ab4810f01b
nehashingote <54305741+nehashingote@users.noreply.github.com>
2019-08-27 13:42:18 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,11 @@
+x=int(input("enter first number"))
+y=int(input("enter second number"))
+print("before swapping")
+print("the value of x",x)
+print("the value of y",y)
+temp=x
+x=y
+y=temp
+print("after swapping")
+print("the value of x",x)
+print("the value of y",y)
